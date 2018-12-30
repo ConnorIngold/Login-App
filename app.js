19089224224bff43b2b973ccca5542f44d6725d8
connoringold <connoringold@gmail.com>
2018-12-30 01:15:38 +0000

    second commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true })
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err));
+
+
+// EJS
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
+// Bodyparser
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
